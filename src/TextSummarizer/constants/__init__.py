ef557da98f1ac9fbdd799e65b6e760841f60a75e
Darshan31Patel <darshan.h3112@gmail.com>
2023-10-05 22:40:34 +0530

    Data Ingestion

---
+++ src/TextSummarizer/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"C:\Users\Darshan\OneDrive\Desktop\Learning\Text_summarization\Text-Summarization\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\Darshan\OneDrive\Desktop\Learning\Text_summarization\Text-Summarization\params.yaml")
+# print(CONFIG_FILE_PATH)
